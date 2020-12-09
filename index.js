0595c40c5ec371ca8e8f936b2eafb41982740c56
Oscar López <oscar.lopez@alibrate.com>
2020-12-09 17:57:59 -0300

    add wewather api simple request

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+const WEATHER_API = 'https://api.openweathermap.org/data/2.5/weather?appid=b1e678cd4eea353040a44e9eaaa70e61&units=metric'
 
 app.get('/', (req, res) => {
   res.json({
     text: 'hola'
   })
+})
+
+app.get('/weather', async (req, res) => {
+  console.log('req', req.query)
+  try {
+    const { data } = await axios.post(`${WEATHER_API}&q=${req.query.city}`)
+    console.log('data', data)
+    res.json(data)
+  } catch (err) {
+    return {
+      err
+    }
+  }
 })
 
 app.get('/health', (req, res) => {
