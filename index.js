fb0a00361081b57f76dfc5b3dfc3fa6539ab1ff2
Oscar López <oscar.lopez@alibrate.com>
2020-12-09 18:06:19 -0300

    add simple cache for testing purpose

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 const WEATHER_API = 'https://api.openweathermap.org/data/2.5/weather?appid=b1e678cd4eea353040a44e9eaaa70e61&units=metric'
-
+const cache = {}
 app.get('/', (req, res) => {
   res.json({
     text: 'hola'
@@ -11,9 +11,14 @@ app.get('/', (req, res) => {
 
 app.get('/weather', async (req, res) => {
   console.log('req', req.query)
+  if (cache[req.query.city]) {
+    console.log('HIT', req.query.city)
+    return res.json(cache[req.query.city])
+  }
   try {
     const { data } = await axios.post(`${WEATHER_API}&q=${req.query.city}`)
     console.log('data', data)
+    cache[req.query.city] = data
     res.json(data)
   } catch (err) {
     return {
